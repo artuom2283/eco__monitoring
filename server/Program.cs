f254896e794c65fbd0c3cbc6244c563ad919a1d9
MirosMazurenko <mazurenkomyroslav@gmail.com>
2024-09-16 19:07:16 +0300

    Services added to Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,8 @@
+using server.Repository;
+using server.Repository.Interfaces;
+using server.Services;
+using server.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+
+builder.Services.AddScoped<IPollutionRepository, PollutionRepository>();
+builder.Services.AddScoped<IPollutionService, PollutionService>();
+builder.Services.AddScoped<IIndustrialFacilityRepository, IndustrialFacilityRepository>();
+builder.Services.AddScoped<IIndustrialFacilityService, IndustrialFacilityService>();
 
 var app = builder.Build();
 
